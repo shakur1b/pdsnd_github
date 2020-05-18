cf3a926d241279d2fe535dcb2e4bff5ae62fc048
shakur1b <65244283+shakur1b@users.noreply.github.com>
2020-05-19 00:33:21 +0300

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -17,7 +17,7 @@ DAYS = ['sunday', 'monday', 'tuesday', 'wednesday','thursday', 'friday', 'saturd
 
 
 def get_filters():
-   hello ='Hello! Let\'s explore some US bikeshare data!
+   hello = "Hello! Let\'s explore some US bikeshare data!"
     print(hello)
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
